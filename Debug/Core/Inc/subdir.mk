59d80ada8ae17b4199fa5f7db711d7389629520d
Samuel Priddy <srpriddy@ad.unc.edu>
2023-09-20 14:56:49 -0400

    Initial commit

---
+++ Debug/Core/Inc/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Inc/STM_MY_LCD16X2.c 
+
+OBJS += \
+./Core/Inc/STM_MY_LCD16X2.o 
+
+C_DEPS += \
+./Core/Inc/STM_MY_LCD16X2.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Inc/%.o Core/Inc/%.su Core/Inc/%.cyclo: ../Core/Inc/%.c Core/Inc/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F407xx -c -I../USB_HOST/App -I../USB_HOST/Target -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Middlewares/ST/STM32_USB_Host_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Inc
+
+clean-Core-2f-Inc:
+	-$(RM) ./Core/Inc/STM_MY_LCD16X2.cyclo ./Core/Inc/STM_MY_LCD16X2.d ./Core/Inc/STM_MY_LCD16X2.o ./Core/Inc/STM_MY_LCD16X2.su
+
+.PHONY: clean-Core-2f-Inc
+
---
+++ Debug/Drivers/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/STM_MY_LCD16X2.c 
+
+OBJS += \
+./Drivers/STM_MY_LCD16X2.o 
+
+C_DEPS += \
+./Drivers/STM_MY_LCD16X2.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/%.o Drivers/%.su Drivers/%.cyclo: ../Drivers/%.c Drivers/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F407xx -c -I../USB_HOST/App -I../USB_HOST/Target -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Middlewares/ST/STM32_USB_Host_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Drivers
+
+clean-Drivers:
+	-$(RM) ./Drivers/STM_MY_LCD16X2.cyclo ./Drivers/STM_MY_LCD16X2.d ./Drivers/STM_MY_LCD16X2.o ./Drivers/STM_MY_LCD16X2.su
+
+.PHONY: clean-Drivers
+
